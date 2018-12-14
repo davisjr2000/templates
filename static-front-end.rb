0ca6871368e2a60481775afbfdb14cce7ee806eb
Davis JR <davisjr2000@hotmail.com>
2018-12-14 15:13:27 -0200

    Remove devise from template

--- static-front-end.rb
+++ static-front-end.rb
@@ -1,1 +1,182 @@
-This is a test
+run 'pgrep spring | xargs kill -9'
+
+# GEMFILE
+########################################
+run 'rm Gemfile'
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+ruby '#{RUBY_VERSION}'
+
+#{"gem 'bootsnap', require: false" if Rails.version >= "5.2"}
+gem 'jbuilder', '~> 2.0'
+gem 'pg', '~> 0.21'
+gem 'puma'
+gem 'rails', '#{Rails.version}'
+gem 'redis'
+
+gem 'autoprefixer-rails'
+gem 'bootstrap-sass', '~> 3.3'
+gem 'font-awesome-sass', '~> 5.5.0'
+gem 'sassc-rails'
+gem 'uglifier'
+gem 'webpacker'
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+end
+
+group :development, :test do
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'dotenv-rails'
+end
+RUBY
+
+# Ruby version
+########################################
+file '.ruby-version', RUBY_VERSION
+
+# Procfile
+########################################
+file 'Procfile', <<-YAML
+web: bundle exec puma -C config/puma.rb
+YAML
+
+# Assets
+########################################
+run 'rm -rf app/assets/stylesheets'
+run 'rm -rf vendor'
+
+run 'rm app/assets/javascripts/application.js'
+file 'app/assets/javascripts/application.js', <<-JS
+//= require jquery
+//= require jquery_ujs
+//= require rails-ujs
+//= require_tree .
+JS
+
+# Dev environment
+########################################
+gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+
+# Layout
+########################################
+run 'rm app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.erb', <<-HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TODO</title>
+    <%= csrf_meta_tags %>
+    <%= action_cable_meta_tag %>
+    <%= stylesheet_link_tag 'application', media: 'all' %>
+    <%#= stylesheet_pack_tag 'application', media: 'all' %> <!-- Uncomment if you import CSS in app/javascript/packs/application.js -->
+  </head>
+  <body>
+    <%= yield %>
+    <%= javascript_include_tag 'application' %>
+    <%= javascript_pack_tag 'application' %>
+  </body>
+</html>
+HTML
+
+# README
+########################################
+markdown_file_content = <<-MARKDOWN
+Rails app generated with HortaLab Static Front-End template.
+MARKDOWN
+file 'README.md', markdown_file_content, force: true
+
+# Generators
+########################################
+generators = <<-RUBY
+config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
+RUBY
+
+environment generators
+
+########################################
+# AFTER BUNDLE
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command 'db:drop db:create db:migrate'
+  generate('--bootstrap')
+  generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
+
+  # Routes
+  ########################################
+  route "root to: 'pages#home'"
+
+  # Git ignore
+  ########################################
+  run 'rm .gitignore'
+  file '.gitignore', <<-TXT
+.bundle
+log/*.log
+tmp/**/*
+tmp/*
+!log/.keep
+!tmp/.keep
+*.swp
+.DS_Store
+public/assets
+public/packs
+public/packs-test
+node_modules
+yarn-error.log
+.byebug_history
+.env*
+TXT
+
+  # migrate + devise views
+  ########################################
+  rails_command 'db:migrate'
+
+  # Environments
+  ########################################
+  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
+
+  # Webpacker / Yarn
+  ########################################
+  run 'rm app/javascript/packs/application.js'
+  run 'yarn add jquery bootstrap@3'
+  file 'app/javascript/packs/application.js', <<-JS
+import "bootstrap";
+JS
+
+  inject_into_file 'config/webpack/environment.js', before: 'module.exports' do
+<<-JS
+// Bootstrap 3 has a dependency over jQuery:
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+
+JS
+  end
+
+  # Dotenv
+  ########################################
+  run 'touch .env'
+
+  # Git
+  ########################################
+  git :init
+  git add: '.'
+  git commit: "-m 'Initial commit with HortaLab static Front-End template'"
+end
